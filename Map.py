f236926b966f1b5d1d6a9799fdecc15eb695c704
JayanthTunk <jayanth.tunk@gmail.com>
2023-04-24 14:40:36 +0530

    Sathyabama Map

---
+++ Map.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+# open method used to open different extension image file
+im = Image.open(r"C:\Users\dell\Desktop\Satbot\Sathyabama Campus map.jpg")
+
+# This method will show image in any image viewer
+im.show()
