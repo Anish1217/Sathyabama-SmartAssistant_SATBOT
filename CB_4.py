9dad51f99b11fe1cd69320d0d8ea0e3c87a4dede
JayanthTunk <jayanth.tunk@gmail.com>
2023-04-24 14:34:17 +0530

    Sathyabama Placement

---
+++ CB_4.py
@@ -0,0 +1,5 @@
+import webbrowser
+# Add a URL of JavaTpoint to open it in a browser
+url = 'https://placement.sathyabama.ac.in/'
+# Open the URL using open() function of module
+webbrowser.open_new_tab(url)
