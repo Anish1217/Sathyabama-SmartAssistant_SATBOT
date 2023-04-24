45f404ce72d6a17ca5e334c162abf1e2c2d35e9c
JayanthTunk <jayanth.tunk@gmail.com>
2023-04-24 14:35:18 +0530

    Sathyabama LMS

---
+++ CB_5.py
@@ -0,0 +1,5 @@
+import webbrowser
+# Add a URL of JavaTpoint to open it in a browser
+url = 'https://sathyabama.cognibot.in/'
+# Open the URL using open() function of module
+webbrowser.open_new_tab(url)
