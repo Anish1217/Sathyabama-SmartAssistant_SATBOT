5a7c25a3df63339a89345b8d93232c68ce91e895
JayanthTunk <jayanth.tunk@gmail.com>
2023-04-24 14:37:04 +0530

    Sathyabama ERP

---
+++ CB_6.py
@@ -0,0 +1,5 @@
+import webbrowser
+# Add a URL of JavaTpoint to open it in a browser
+url = "https://erp.sathyabama.ac.in/account/login?returnUrl=%2F/ "
+# Open the URL using open() function of module
+webbrowser.open_new_tab(url)
